6341c38a953b90e5b5d941ed755c56f3073c2c3c
Gouson <gouson@qq.com>
2021-03-05 17:47:34 +0800

    deploy

---
+++ precache-manifest.26b9acd803c275cab847915933e5e62d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2570ba650a8b139ecbf",
+    "url": "/shaving-ice-bookkeeping-website/css/app.ca7bf14e.css"
+  },
+  {
+    "revision": "27cb2544d55591ac334d21f85f13894a",
+    "url": "/shaving-ice-bookkeeping-website/index.html"
+  },
+  {
+    "revision": "c2570ba650a8b139ecbf",
+    "url": "/shaving-ice-bookkeeping-website/js/app.6b7c13e2.js"
+  },
+  {
+    "revision": "a5b1a08e36766e3e1a51",
+    "url": "/shaving-ice-bookkeeping-website/js/chunk-vendors.91800239.js"
+  },
+  {
+    "revision": "268ccdc1da4908fabc5edc52d5b65a11",
+    "url": "/shaving-ice-bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shaving-ice-bookkeeping-website/robots.txt"
+  }
+]);
